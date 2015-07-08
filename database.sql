2d49415d17da2aed6031105591fa31aeae588dfa
Liuta Ovidiu <thinkovi@Liutas-MacBook-Pro.local>
2015-07-08 23:31:15 +0300

    sql update

--- database.sql
+++ database.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `posts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
-  `author` varchar(100) DEFAULT NULL,
   `content` text,
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL,
+  `permalink` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
